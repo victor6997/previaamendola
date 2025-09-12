338d9d551801b4e8adbcad105c16f1895858498d
victor6997 <114714616+victor6997@users.noreply.github.com>
2025-09-12 15:26:46 -0300

    alteracao

---
+++ js/preloader.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() {
+  // Quando a página estiver completamente carregada, o preloader é escondido
+  document.querySelector('.loadingspinner').style.display = 'none';
+});
